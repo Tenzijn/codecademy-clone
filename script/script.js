645071f3a23c30487c9aaf8cf74c47b5303ad8ca
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2023-11-19 14:48:17 +0100

    saving from mac book

--- script/script.js
+++ script/script.js
@@ -1,24 +1,4 @@
-/*
-// adding notice bar with js
-const noticeText =
-  "Summer's over - what's your move? Join Pro for 50% off using code";
-const offerCode = 'CODE50%';
-const messageContainer = document.createElement('div');
-messageContainer.classList.add('notice');
-
-const message = document.createElement('div');
-message.classList.add('noticeContainer');
-message.classList.add('container');
-message.innerHTML = ` <p class="container"> ${noticeText} <b> ${offerCode}</b> </p> <ion-icon name="close" class="right cursor close"></ion-icon>`;
-const nav = document.querySelector('nav');
-
-messageContainer.append(message);
-nav.before(messageContainer);
-
-document.querySelector('.close').addEventListener('click', function () {
-  messageContainer.remove();
-});
-*/
+'use strict';
 
 // variables
 const noticeBar = document.querySelector('.notice');
@@ -28,6 +8,105 @@ const video = document.querySelector('.video');
 const navItem = document.querySelectorAll('.nav__link');
 const navContent = document.querySelectorAll('.navbar__content__menu--item');
 const navMenuIcon = document.querySelector('.navMenuIcon');
+const mobileNav = document.querySelector('.mobile__nav');
+const mobileNavClose = document.querySelector('.closeMobileNav');
+const scrollBtn = document.querySelector('.scrollTop');
+const headerSection = document.querySelector('.header');
+const courseSection = document.querySelector('.courses');
+const navAi = document.querySelector('.courses__ai');
+const navFree = document.querySelector('.courses__free');
+const navNewCoding = document.querySelector('.courses__newCoding');
+const navMostPopular = document.querySelector('.courses__mostPopular');
+const navSkillPath = document.querySelector('.courses__skillPath');
+const navCareerPath = document.querySelector('.courses__careerPath');
+const courseCard = document.querySelectorAll('.cards');
+const courseNavItem = document.querySelectorAll(
+  '.courses__content__nav__navMenu--navItem'
+);
+
+// courses navigation carousel
+
+// document.querySelectorAll('.course__card').forEach((e) => {
+//   console.log(
+//     e.classList.forEach((className) =>
+//       className == 'ai' ? console.log(className) : console.log('not ai')
+//     )
+//   );
+// });
+
+// Working on it
+courseNavItem.forEach((navItem) => {
+  navItem.addEventListener('click', (e) =>
+    e.target.classList.forEach((navClassName) => {
+      console.log(navClassName);
+      courseCard.forEach((course) => {
+        if (navClassName == course) {
+          console.log('matched');
+        }
+      });
+
+      // console.log(`nav class name : ${navClassName}`);
+      // courseCard.forEach((card) => {
+      //   card.classList.forEach((cardClass) => {
+      //     console.log(navClassName);
+      //     console.log(cardClass);
+      //     if (cardClass == navClassName) {
+      //       // console.log(cardClass);
+      //       // card.classList.toggle('hidden');
+      //       console.log('matched');
+      //     }
+      //     // } else {
+      //     // card.remove();
+      //     // }
+      //   });
+      // });
+    })
+  );
+});
+
+// courseNavItem.forEach((navItem) => {
+//   navItem.addEventListener('click', (e) =>
+//     e.target.classList.forEach((navClassName) => {
+//       courseCard.forEach((card) => {
+//         card.classList.forEach((cardClass) => {
+//           // console.log(navClassName);
+//           // console.log(cardClass);
+//           if (cardClass == navClassName) {
+//             card.classList.toggle('hidden');
+//             console.log('matched');
+//           }
+//           // } else {
+//           // card.remove();
+//           // }
+//         });
+//       });
+//     })
+//   );
+// });
+
+// navAi.addEventListener('click', () => {
+//   courseCard.forEach((card) => {
+//     card.classList.forEach((cardClass) => {
+//       if (cardClass === 'ai') {
+//         card.classList.add('show');
+//       } else {
+//         card.remove();
+//       }
+//     });
+//   });
+// });
+
+// navFree.addEventListener('click', () => {
+//   courseCard.forEach((card) => {
+//     card.classList.forEach((cardClass) => {
+//       if (cardClass === 'free') {
+//         card.classList.add('show');
+//       } else {
+//         card.remove();
+//       }
+//     });
+//   });
+// });
 
 // closing the Notice bar
 noticeCloseBtn.addEventListener('click', () => {
@@ -72,11 +151,65 @@ navContent.forEach((item) =>
   )
 );
 
-// navContent.forEach((item) =>
-//   item.addEventListener(
-//     'mouseout',
-//     () => (item.lastElementChild.style.display = 'none')
-//   )
-// );
+// mobile nav
+navMenuIcon.addEventListener(
+  'click',
+  () => (mobileNav.style.display = 'block')
+);
 
-navMenuIcon.addEventListener('click', () => console.log('hello'));
+mobileNavClose.addEventListener(
+  'click',
+  () => (mobileNav.style.display = 'none')
+);
+
+// for scroll function and btn
+window.addEventListener('scroll', function (e) {
+  this.scrollY === 0
+    ? (scrollBtn.style.display = 'none')
+    : (scrollBtn.style.display = 'block');
+});
+
+scrollBtn.addEventListener('click', () =>
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+);
+
+/*
+// adding notice bar with js
+const noticeText =
+  "Summer's over - what's your move? Join Pro for 50% off using code";
+const offerCode = 'CODE50%';
+const messageContainer = document.createElement('div');
+messageContainer.classList.add('notice');
+
+const message = document.createElement('div');
+message.classList.add('noticeContainer');
+message.classList.add('container');
+message.innerHTML = ` <p class="container"> ${noticeText} <b> ${offerCode}</b> </p> <ion-icon name="close" class="right cursor close"></ion-icon>`;
+const nav = document.querySelector('nav');
+
+messageContainer.append(message);
+nav.before(messageContainer);
+
+document.querySelector('.close').addEventListener('click', function () {
+  messageContainer.remove();
+});
+*/
+
+// Intersection Observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((e) => {
+//     console.log(e);
+//   });
+// };
+
+// const obsOpts = {
+//   root: null,
+//   threshold: 0.1,
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOpts);
+// observer.observe(courseSection);
